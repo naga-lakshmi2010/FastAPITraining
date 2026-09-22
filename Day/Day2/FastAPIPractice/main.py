6c40774d9578b80015ff87d5610ce7111ef53345
naga-lakshmi2010 <2025cs_charukunagalakshmi_b@nie.ac.in>
2026-09-22 12:53:50 +0530

    Adding Multiple roots

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
-def Charitha():
-    return {"message":"hello world","number":44,"is_fun":True}
+def home():
+    return {"page":"home"}
+@app.get("/about")
+def about():
+    return{"page":"About","author":"Charitha"}
+@app.get("/health")
+def health():
+    return{"status":"ok"}
